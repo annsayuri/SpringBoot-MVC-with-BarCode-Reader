ba96f400689d42d4621a25945484a6c4e1960cc4
annsayuri <annsayu12@gmail.com>
2026-08-05 22:44:12 +0530

    Create GRN services

--- src/main/java/com/bci/productcrud/service/GRNService.java
+++ src/main/java/com/bci/productcrud/service/GRNService.java
@@ -1,5 +1,10 @@
 package com.bci.productcrud.service;
 
-public class GRNService {
-    
+import com.bci.productcrud.model.GRN;
+import java.util.List;
+
+public interface GRNService {
+    List<GRN> getAllGRNs();
+    GRN getGRNById(Long id);
+    GRN createGRN(GRN grn);
 }
