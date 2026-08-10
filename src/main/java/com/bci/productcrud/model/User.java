b3379a57bceafb2dbb7ee020daccbec94bdd7270
annsayuri <annsayu12@gmail.com>
2026-08-10 18:52:27 +0530

    Create a user model

---
+++ src/main/java/com/bci/productcrud/model/User.java
@@ -0,0 +1,38 @@
+package com.bci.productcrud.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(name = "contact_no")
+    private String contactNo;
+
+    private String status;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+}
